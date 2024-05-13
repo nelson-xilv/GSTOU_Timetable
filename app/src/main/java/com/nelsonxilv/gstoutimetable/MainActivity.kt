6160c3ede3fa83b1eca07dbdc3eb75b05b494cbe
neslonXILV <khojugken@gmail.com>
2024-05-13 11:29:02 +0300

    feat: enable edge-to-edge

--- app/src/main/java/com/nelsonxilv/gstoutimetable/MainActivity.kt
+++ app/src/main/java/com/nelsonxilv/gstoutimetable/MainActivity.kt
@@ -3,6 +3,7 @@ package com.nelsonxilv.gstoutimetable
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
@@ -13,6 +14,7 @@ import com.nelsonxilv.gstoutimetable.presentation.theme.GSTOUTimetableTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContent {
             GSTOUTimetableTheme {
