55cd2a4e6f3e49f27d8cbe7710314c756edf4130
neslonXILV <khojugken@gmail.com>
2024-05-17 11:28:30 +0300

    feat: change function of getting the schedule from the repository

--- app/src/main/java/com/nelsonxilv/gstoutimetable/presentation/screens/TimetableViewModel.kt
+++ app/src/main/java/com/nelsonxilv/gstoutimetable/presentation/screens/TimetableViewModel.kt
@@ -2,50 +2,35 @@ package com.nelsonxilv.gstoutimetable.presentation.screens
 
 import android.icu.text.SimpleDateFormat
 import android.icu.util.Calendar
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.nelsonxilv.gstoutimetable.data.TimetableRepository
-import com.nelsonxilv.gstoutimetable.data.network.entity.LessonDto
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
-import retrofit2.HttpException
-import java.io.IOException
 import java.util.Locale
 
 class TimetableViewModel : ViewModel() {
 
     private val repository = TimetableRepository()
 
-    private val _schedule: MutableStateFlow<List<LessonDto>> = MutableStateFlow(emptyList())
-    val schedule: StateFlow<List<LessonDto>>
-        get() = _schedule.asStateFlow()
-
-    var timetableUiState: TimetableUiState by mutableStateOf(TimetableUiState.Loading)
-        private set
+    private val _timetableUiState = MutableStateFlow<TimetableUiState>(TimetableUiState.Loading)
+    val timetableUiState: StateFlow<TimetableUiState> = _timetableUiState
 
     init {
-        getTimetable()
+        getTodaySchedule()
     }
 
-    private fun getTimetable() {
+    private fun getTodaySchedule() {
         viewModelScope.launch {
-            timetableUiState = TimetableUiState.Loading
-            timetableUiState = try {
-                val lessons = repository.getSchedule()
-                _schedule.value = lessons
-                TimetableUiState.Success(
+            _timetableUiState.value = TimetableUiState.Loading
+            try {
+                _timetableUiState.value = TimetableUiState.Success(
                     date = getCurrentDate(),
-                    lessons = "Success: ${schedule.value.size} lessons retrieved"
+                    lessons = repository.getTodaySchedule()
                 )
-            } catch (e: IOException) {
-                TimetableUiState.Error
-            } catch (e: HttpException) {
-                TimetableUiState.Error
+            } catch (e: Exception) {
+                _timetableUiState.value = TimetableUiState.Error
             }
         }
     }
