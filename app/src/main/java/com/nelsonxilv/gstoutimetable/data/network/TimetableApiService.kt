a8298c4585339c632e1dc1a8faec102e741a2fdf
neslonXILV <khojugken@gmail.com>
2024-05-17 11:24:34 +0300

    Revert "feat: add a function to receive lessons for today"
    This reverts commit a0ae10298cc173c87662590be4fff011ba0919fe.

--- app/src/main/java/com/nelsonxilv/gstoutimetable/data/network/TimetableApiService.kt
+++ app/src/main/java/com/nelsonxilv/gstoutimetable/data/network/TimetableApiService.kt
@@ -1,10 +1,10 @@
 package com.nelsonxilv.gstoutimetable.data.network
 
-import com.nelsonxilv.gstoutimetable.data.network.model.LessonDto
+import com.nelsonxilv.gstoutimetable.data.network.entity.LessonDto
 import retrofit2.http.GET
 
 interface TimetableApiService {
 
-    @GET("api/timetable/public/entrie/?group=ист-23-3")
+    @GET("api/timetable/public/entrie/?group=ист-23-1э")
     suspend fun getSchedule(): List<LessonDto>
 }
