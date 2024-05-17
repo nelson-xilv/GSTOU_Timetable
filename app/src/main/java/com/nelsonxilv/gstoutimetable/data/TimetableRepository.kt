8eeb0f921e4e3519a033a72d4378fe629d4721bf
neslonXILV <khojugken@gmail.com>
2024-05-17 11:24:52 +0300

    feat: add a function to receive lessons for today

--- app/src/main/java/com/nelsonxilv/gstoutimetable/data/TimetableRepository.kt
+++ app/src/main/java/com/nelsonxilv/gstoutimetable/data/TimetableRepository.kt
@@ -1,11 +1,42 @@
 package com.nelsonxilv.gstoutimetable.data
 
-import com.nelsonxilv.gstoutimetable.data.network.entity.LessonDto
+import android.util.Log
+import com.nelsonxilv.gstoutimetable.data.mapper.LessonMapper
+import com.nelsonxilv.gstoutimetable.data.model.Lesson
 import com.nelsonxilv.gstoutimetable.data.network.TimetableApiFactory
+import java.time.LocalDate
+import java.time.temporal.WeekFields
+import java.util.Locale
 
 class TimetableRepository {
 
-    suspend fun getSchedule(): List<LessonDto> {
-        return TimetableApiFactory.retrofitService.getSchedule()
+    private val lessonMapper = LessonMapper()
+
+    suspend fun getTodaySchedule(): List<Lesson> {
+        val allLesson = getSchedule()
+        val dayOfWeek = LocalDate.now().dayOfWeek
+
+        val dayOfWeekNumber = dayOfWeek.get(WeekFields.of(Locale.getDefault()).dayOfWeek())
+        val lessonList = allLesson.filter { lesson -> lesson.dayOfWeek == dayOfWeekNumber }
+
+        Log.d(TAG, "Day of week: $dayOfWeek\n" +
+                "Number day of week: $dayOfWeekNumber\n" +
+                "Lesson list: $lessonList")
+
+        return lessonList
+    }
+
+    private suspend fun getSchedule(): List<Lesson> {
+        val lessonDtoList = TimetableApiFactory.retrofitService.getSchedule()
+        Log.d(TAG, "Lesson DTO list: $lessonDtoList")
+
+        val lessonList = lessonDtoList.map { dto -> lessonMapper.mapDtoToModel(dto) }
+        Log.d(TAG, "Lesson list: $lessonList")
+
+        return lessonList
+    }
+
+    companion object {
+        private const val TAG = "TimetableRepository"
     }
 }
