a0ae10298cc173c87662590be4fff011ba0919fe
neslonXILV <khojugken@gmail.com>
2024-05-17 11:23:57 +0300

    feat: add a function to receive lessons for today

--- app/src/main/java/com/nelsonxilv/gstoutimetable/data/network/TimetableApiService.kt
+++ app/src/main/java/com/nelsonxilv/gstoutimetable/data/network/TimetableApiService.kt
@@ -1,10 +1,10 @@
 package com.nelsonxilv.gstoutimetable.data.network
 
-import com.nelsonxilv.gstoutimetable.data.network.entity.LessonDto
+import com.nelsonxilv.gstoutimetable.data.network.model.LessonDto
 import retrofit2.http.GET
 
 interface TimetableApiService {
 
-    @GET("api/timetable/public/entrie/?group=ист-23-1э")
+    @GET("api/timetable/public/entrie/?group=ист-23-3")
     suspend fun getSchedule(): List<LessonDto>
 }
