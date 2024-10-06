a47ea9142c63e7d97abbde3aa7c5df1f73fca9be
nelson-XILV <khojugken@gmail.com>
2024-10-06 20:22:41 +0300

    refactor: improve dispatcher handling

--- app/src/main/java/com/nelsonxilv/gstoutimetable/data/TimetableRepositoryImpl.kt
+++ app/src/main/java/com/nelsonxilv/gstoutimetable/data/TimetableRepositoryImpl.kt
@@ -6,46 +6,55 @@ import com.nelsonxilv.gstoutimetable.data.mapper.MapperService
 import com.nelsonxilv.gstoutimetable.data.model.GroupDbModel
 import com.nelsonxilv.gstoutimetable.data.model.LessonDbModel
 import com.nelsonxilv.gstoutimetable.data.network.TimetableApiService
+import com.nelsonxilv.gstoutimetable.di.Dispatcher
+import com.nelsonxilv.gstoutimetable.di.TimetableDispatchers
 import com.nelsonxilv.gstoutimetable.domain.TimetableRepository
 import com.nelsonxilv.gstoutimetable.domain.entity.Group
 import com.nelsonxilv.gstoutimetable.domain.entity.Lesson
-import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class TimetableRepositoryImpl @Inject constructor(
     private val apiService: TimetableApiService,
     private val timetableDao: TimetableDao,
-    private val mapper: MapperService
+    private val mapper: MapperService,
+    @Dispatcher(TimetableDispatchers.IO) private val ioDispatcher: CoroutineDispatcher
 ) : TimetableRepository {
 
     override suspend fun getLessonList(groupName: String): Flow<List<Lesson>> {
+        var lessonsDb: List<LessonDbModel>
         val localData = timetableDao.getGroupWithLessons(groupName)
 
         return flow {
+
             if (localData == null) {
-                val lessonDbModels = getScheduleFromApi(groupName)
-                cacheLessons(groupName, lessonDbModels)
-                val lessons = mapper.mapListLessonDbToListEntity(lessonDbModels)
-                emit(lessons)
+                lessonsDb = getScheduleFromApi(groupName)
+                cacheLessons(groupName, lessonsDb)
             } else {
-                val lessons = mapper.mapListLessonDbToListEntity(localData.lessons)
-                emit(lessons)
+                lessonsDb = localData.lessons
             }
-        }.flowOn(Dispatchers.IO)
+
+            val lessons = mapper.mapListLessonDbToListEntity(lessonsDb)
+            emit(lessons)
+
+        }.flowOn(ioDispatcher)
     }
 
     override suspend fun deleteGroupAndLessons(groupName: String) {
-        timetableDao.deleteGroupWithLessons(groupName)
+        withContext(ioDispatcher) {
+            timetableDao.deleteGroupWithLessons(groupName)
+        }
     }
 
     override fun getGroupList(): Flow<List<Group>> =
         timetableDao.getAllGroups().map { listGroupDb ->
             mapper.mapListGroupDbToListEntity(listGroupDb)
-        }
+        }.flowOn(ioDispatcher)
 
     private suspend fun getScheduleFromApi(groupName: String): List<LessonDbModel> {
         val lessonDtoList = apiService.getSchedule(groupName)
